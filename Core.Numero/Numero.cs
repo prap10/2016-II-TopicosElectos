a4ab25876caf197b412eb84e8bcd76862b262b07
parroyo <prap10.96@gmail.com>
2016-05-21 16:19:04 -0600

    First Commit - Number class and its properties

---
+++ Core.Numero/Numero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Numero
+{
+    public class Numero
+    {
+        public string elNumero { get; set; }
+        public int laBase { get; set; }
+
+        public bool esBaseDiez { get {
+                return laBase == 10; }
+            }
+
+        public double elNumeroEnDecimal { get {
+                return double.Parse(elNumero); }
+            }
+
+    }
+}
